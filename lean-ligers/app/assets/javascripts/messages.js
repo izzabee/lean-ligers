d54479d8dda00c715fa842f5759a37b971ae8db1
Izabella Stern <izabellastern@gmail.com>
2015-03-16 19:38:08 -0400

    fixed message ajax

--- lean-ligers/app/assets/javascripts/messages.js
+++ lean-ligers/app/assets/javascripts/messages.js
@@ -11,12 +11,14 @@ $(document).ready(function(){
 	
 	$form.hide();
 
+	// this shows the message form
 	$getMessage.on('click', function(e){
 		$(this).next().show();
 		$closeForm.on('click', function(e){
 			$form.hide();
 		});
 
+		// this gets the message from the form
 		$sendMessage.on('click', function(e){
 			e.preventDefault();
 			formElements = $form[0].elements;
@@ -29,35 +31,40 @@ $(document).ready(function(){
 				}
 			};
 
+			// and this sends it to server
 			$.ajax({
 				url: '/messages',
 				type: 'POST',
 				dataType: 'json',
 				data: formData,
 				success: function(formData){
-					console.log(formData);
+					$form[0].reset();
+					$form.hide();
 				}
 			})
 		})
 	});
 
-	// var populateMessages = function(){
-	// 	var that = this;
+	var populateMessages = function(){
+		var $conversation = $('.conversation')
+		var $subject = $('.subject');
+		var $body = $('.body');
 
-	// 	$.ajax({
-	// 		url: '/messages',
-	// 		method: 'GET',
-	// 		dataType: 'json',
-	// 		success: function(returnedData){
-	// 			console.log(returnedData);
-	// 		}
-	// 	})
-	// }
+		$.ajax({
+			url: '/messages',
+			method: 'GET',
+			dataType: 'json',
+			success: function(returnedData){
+				 $subject.text(returnedData.subject);
+				 $body.text(returnedData.content);
+			}
+		})
+	}
 
 
 
 
-
+	populateMessages();
 
 
 
